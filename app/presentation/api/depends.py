de8e87471b01c77438a2f6eb7296755fe3edb34d
Squilix01 <Squilix01@no-reply.com>
2025-11-15 21:17:21 +0200

    feat(presentation): add depends.py for override

---
+++ app/presentation/api/depends.py
@@ -0,0 +1,8 @@
+
+
+async def translation_repo():
+    raise NotImplementedError("Должен быть переопределён в слое инфры")
+
+
+async def cgpt_repo():
+    raise NotImplementedError("Должен быть переопределён в слое инфры")
